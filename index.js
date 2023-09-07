d7ab6383e66b7fef6d5458ead4c313e08930f9e8
unknown <athiraathiur10@gmail.com>
2023-09-07 12:26:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+ window.onscroll=function(){myfunction()};
+ var top1 =getElementsById("top1-1");
+ var sticky=top1.offsetTop;
+ function myfunction(){
+  if (window.pageYoffset >=sticky){
+    top1.classList.add("sticky")
+
+  } else{
+    top1.classList.remove("sticky");
+  }
+
+ }
